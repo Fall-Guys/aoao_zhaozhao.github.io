7efe38b843de96dd5c77747654a946672986efc7
aoao_zhaozhao <114546607+aoao-zhaozhao@users.noreply.github.com>
2025-05-11 14:19:58 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // 等待DOM加载完成后执行
+    const button = document.getElementById('myButton');
+    
+    button.addEventListener('click', function() {
+        alert('按钮被点击了！');
+    });
+});
+    
